e2668139c114656a1ea73888c2ea99a55c381fd5
sandjio <emmasandjio@gmail.com>
2024-04-24 00:24:18 +0100

    Added the dashboard page.tsx and layout.tsx

---
+++ src/app/dashboard/layout.tsx
@@ -0,0 +1,14 @@
+import { Montserrat } from "next/font/google";
+const montserrat = Montserrat({ weight: ["400"], subsets: ["latin"] });
+
+export default function RootLayout({
+  children,
+}: Readonly<{
+  children: React.ReactNode;
+}>) {
+  return (
+    <html lang="en">
+      <body className={montserrat.className}>{children}</body>
+    </html>
+  );
+}
