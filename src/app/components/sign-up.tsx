652b39d370ef3bc9551cfac70447b171711208a3
sandjio <emmasandjio@gmail.com>
2024-04-24 00:26:39 +0100

    Implemented the signup logic in the sign-up form

--- src/app/components/sign-up.tsx
+++ src/app/components/sign-up.tsx
@@ -1,6 +1,33 @@
+//TODO: Implement a function to validate that the password and the confirm password matches
+// TODO: Before redirecting the user user to the login show an animation to confirm signup
+// TODO: Change the button to a component
+"use client";
+
+import Link from "next/link";
+import React, { useState } from "react";
+import { useRouter } from "next/navigation";
+
+import { signUpUser } from "../lib/actions";
+
 export default function SignUpForm() {
+  const router = useRouter();
+  const [firstName, setFirstName] = useState("");
+  const [lastName, setLastName] = useState("");
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
+
+  const handleSubmit = (e: any) => {
+    e.preventDefault();
+    signUpUser(email, firstName, lastName, password);
+  };
+
   return (
-    <form className="flex flex-col items-center" action="#" method="post">
+    <form
+      onSubmit={handleSubmit}
+      className="flex flex-col items-center"
+      action=""
+      method="post"
+    >
       <div className="flex flex-col  space-y-2">
         <label className="text-lg" htmlFor="first-name">
           First Name
@@ -10,6 +37,8 @@ export default function SignUpForm() {
           type="text"
           placeholder="Enter first name"
           name="first-name"
+          required
+          onChange={(e) => setFirstName(e.target.value)}
         />
         <label className="text-lg" htmlFor="last-name">
           Last Name
@@ -19,6 +48,8 @@ export default function SignUpForm() {
           type="text"
           placeholder="Enter last name"
           name="last-name"
+          required
+          onChange={(e) => setLastName(e.target.value)}
         />
         <label className="text-lg" htmlFor="email">
           Email
@@ -28,6 +59,8 @@ export default function SignUpForm() {
           type="email"
           placeholder="Enter email"
           name="email"
+          required
+          onChange={(e) => setEmail(e.target.value)}
         />
         <label className="text-lg" htmlFor="password">
           Password
@@ -37,6 +70,8 @@ export default function SignUpForm() {
           type="password"
           placeholder="Enter password"
           name="password"
+          required
+          onChange={(e) => setPassword(e.target.value)}
         />
         <label className="text-lg" htmlFor="confirm-password">
           Confirm Password
@@ -46,19 +81,21 @@ export default function SignUpForm() {
           type="password"
           placeholder="Confirm password"
           name="confirm-password"
+          required
         />
       </div>
       <button
         className="my-8 rounded-lg border bg-blue-600 px-10 py-2 text-white hover:bg-blue-500"
         type="submit"
+        onClick={() => router.push("/login")}
       >
         Sign Up
       </button>
       <p className="text-sm">
-        Already having an account?{" "}
-        <a className="font-medium hover:underline " href="/login">
+        Already having an account?
+        <Link className="font-medium hover:underline " href="/login">
           Login
-        </a>
+        </Link>
       </p>
     </form>
   );
--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,6 +1,6 @@
 import { Montserrat } from "next/font/google";
 import "./globals.css";
-const inter = Montserrat({ weight: ["400"], subsets: ["latin"] });
+const montserrat = Montserrat({ weight: ["400"], subsets: ["latin"] });
 
 export default function RootLayout({
   children,
@@ -9,7 +9,7 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="en">
-      <body className={inter.className}>{children}</body>
+      <body className={montserrat.className}>{children}</body>
     </html>
   );
 }
