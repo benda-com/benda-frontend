3261aeea9aba5dd5d486f7df827ac7fecd2ae7c2
sandjio <emmasandjio@gmail.com>
2024-04-28 00:42:03 +0100

    Ajusted the navbar to match the design

--- src/app/components/nav.tsx
+++ src/app/components/nav.tsx
@@ -4,15 +4,36 @@ import BendaLogo from "./benda-logo";
 
 export default function Nav() {
   return (
-    <nav className="flex w-full items-center border pl-10">
+    <nav className="flex w-full items-center justify-between  border pl-10">
       <BendaLogo />
-      <ul className="flex  space-x-4 pl-10">
-        <Link href="/">Home</Link>
-        <Link href="/">Products</Link>
-        <Link href="/">Pricing</Link>
-        <Link href="/login">Log in</Link>
-        <Link href="/sign-up">Sign Up</Link>
+      <ul className="flex space-x-10 pl-10">
+        <Link className="text-blue-primary text-lg" href="/">
+          Home
+        </Link>
+        <Link className="text-blue-primary text-lg" href="/">
+          Products
+        </Link>
+        <Link className="text-blue-primary text-lg" href="/">
+          Pricing
+        </Link>
+        <Link className="text-blue-primary text-lg" href="/">
+          About
+        </Link>
       </ul>
+      <span className="space-x-10 pr-10">
+        <Link
+          href="/login"
+          className="rounded-3xl border bg-[#1C1678] px-10 py-2 text-white hover:bg-blue-800"
+        >
+          Log in
+        </Link>
+        <Link
+          href="/sign-up"
+          className="bg-blue-primary rounded-3xl border px-10 py-2 text-white hover:bg-blue-500"
+        >
+          Sign Up
+        </Link>
+      </span>
     </nav>
   );
 }
