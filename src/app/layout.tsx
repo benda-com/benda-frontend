5cab3efaf6ee8122cb48fd7dd6975fa877fff9e2
sandjio <emmasandjio@gmail.com>
2024-04-29 00:30:53 +0100

    Added font awesome icons links in script

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,7 +1,9 @@
 import { Arimo } from "next/font/google";
-import "./globals.css";
-const arimo = Arimo({ weight: ["400"], subsets: ["latin"] });
+import Script from "next/script";
 
+import "./globals.css";
+
+const arimo = Arimo({ weight: ["400"], subsets: ["latin"] });
 export default function RootLayout({
   children,
 }: Readonly<{
@@ -9,6 +11,12 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="en">
+      <head>
+        <Script
+          src="https://kit.fontawesome.com/dd8b004c53.js"
+          crossOrigin="anonymous"
+        ></Script>
+      </head>
       <body className={arimo.className}>{children}</body>
     </html>
   );
